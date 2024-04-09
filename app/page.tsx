06c917c15d615b5f72a204095b542687422337c1
xseignard <xavier.seignard@gmail.com>
2024-04-09 13:15:42 +0200

    enh: added plat

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,24 @@ type FormState = {
   eauConsommee: number;
   prixElec: number;
   cop: number;
+  kwhAnnuelPlat: number;
   prixBallonElec: number;
+  prixBallonPlat: number;
   prixBallonThermo: number;
+  kwhElec?: number;
   coutElec?: number;
+  coutPlat?: number;
+  kwThermo?: number;
   coutThermo?: number;
-  roi?: {
+  roiElecPlat?: {
+    years: number;
+    months: number;
+  };
+  roiElecThermo?: {
+    years: number;
+    months: number;
+  };
+  roiPlatThermo?: {
     years: number;
     months: number;
   };
@@ -30,14 +43,19 @@ type Actions =
         | "SET_EAU_CONSOMMEE"
         | "SET_PRIX_ELEC"
         | "SET_COP"
+        | "SET_KWH_ANNUEL_PLAT"
         | "SET_PRIX_BALLON_ELEC"
+        | "SET_PRIX_BALLON_PLAT"
         | "SET_PRIX_BALLON_THERMO"
+        | "SET_KWH_ELEC"
         | "SET_COUT_ELEC"
+        | "SET_COUT_PLAT"
+        | "SET_KW_THERMO"
         | "SET_COUT_THERMO";
       payload: number;
     }
   | {
-      type: "SET_ROI";
+      type: "SET_ROI_ELEC_THERMO" | "SET_ROI_ELEC_PLAT" | "SET_ROI_PLAT_THERMO";
       payload: {
         years: number;
         months: number;
@@ -61,18 +79,32 @@ const reducer = (state: FormState, action: Actions) => {
       return { ...state, prixElec: action.payload };
     case "SET_COP":
       return { ...state, cop: action.payload };
+    case "SET_KWH_ANNUEL_PLAT":
+      return { ...state, kwhAnnuelPlat: action.payload };
     case "SET_PRIX_BALLON_ELEC":
       return { ...state, prixBallonElec: action.payload };
+    case "SET_PRIX_BALLON_PLAT":
+      return { ...state, prixBallonPlat: action.payload };
     case "SET_PRIX_BALLON_THERMO":
       return { ...state, prixBallonThermo: action.payload };
 
     // outputs
+    case "SET_KWH_ELEC":
+      return { ...state, kwhElec: action.payload };
     case "SET_COUT_ELEC":
       return { ...state, coutElec: action.payload };
+    case "SET_COUT_PLAT":
+      return { ...state, coutPlat: action.payload };
+    case "SET_KW_THERMO":
+      return { ...state, kwThermo: action.payload };
     case "SET_COUT_THERMO":
       return { ...state, coutThermo: action.payload };
-    case "SET_ROI":
-      return { ...state, roi: action.payload };
+    case "SET_ROI_ELEC_PLAT":
+      return { ...state, roiElecPlat: action.payload };
+    case "SET_ROI_ELEC_THERMO":
+      return { ...state, roiElecThermo: action.payload };
+    case "SET_ROI_PLAT_THERMO":
+      return { ...state, roiPlatThermo: action.payload };
     default:
       return state;
   }
@@ -87,7 +119,9 @@ const Home = () => {
     eauConsommee: 150,
     prixElec: 0.2068,
     cop: 3.55,
+    kwhAnnuelPlat: 1440,
     prixBallonElec: 600,
+    prixBallonPlat: 750,
     prixBallonThermo: 2000,
   });
 
@@ -101,7 +135,9 @@ const Home = () => {
       eauConsommee,
       prixElec,
       cop,
+      kwhAnnuelPlat,
       prixBallonElec,
+      prixBallonPlat,
       prixBallonThermo,
     } = state;
     // Température de l'eau avant chauffe
@@ -112,25 +148,61 @@ const Home = () => {
     // Delta chauffe
     const deltaChauffe = eauChaude - tempAvantChauffe;
     // Energie nécessaire chauffe eau classique
-    const energie = deltaChauffe * volume * facteurElec * 365;
+    const khwElecFloat =
+      (deltaChauffe * volume * facteurElec * 365) / (1000 * 1000);
+    const kwhElec = parseFloat(khwElecFloat.toFixed(2));
+    dispatch({ type: "SET_KWH_ELEC", payload: kwhElec });
+    // Energie nécessaire chauffe eau thermodynamique
+    const kwThermo = parseFloat((khwElecFloat / cop).toFixed(2));
+    dispatch({ type: "SET_KW_THERMO", payload: kwThermo });
     // Coût annuel
-    const cout = parseFloat(((energie / (1000 * 1000)) * prixElec).toFixed(2));
-    dispatch({ type: "SET_COUT_ELEC", payload: cout });
-    const coutThermo = parseFloat((cout / cop).toFixed(2));
+    const coutElec = parseFloat((kwhElec * prixElec).toFixed(2));
+    dispatch({ type: "SET_COUT_ELEC", payload: coutElec });
+    const coutPlat = parseFloat((kwhAnnuelPlat * prixElec).toFixed(2));
+    dispatch({ type: "SET_COUT_PLAT", payload: coutPlat });
+    const coutThermo = parseFloat((kwThermo * prixElec).toFixed(2));
     dispatch({ type: "SET_COUT_THERMO", payload: coutThermo });
-    // ROI
-    const deltaPrix = prixBallonThermo - prixBallonElec;
-    const economieAnnuelle = cout - coutThermo;
-    const roiFloat = parseFloat((deltaPrix / economieAnnuelle).toFixed(2));
-    const roiYears = Math.floor(roiFloat);
-    const roiMonths = Math.round((roiFloat - roiYears) * 12);
+    // ROI ELEC PLAT
+    const deltaPrixPlat = prixBallonPlat - prixBallonElec;
+    const economieAnnuellePlat = coutElec - coutPlat;
+    const roiFloatPlat = parseFloat(
+      (deltaPrixPlat / economieAnnuellePlat).toFixed(2)
+    );
+    const roiYearsPlat = Math.floor(roiFloatPlat);
+    const roiMonthsPlat = Math.round((roiFloatPlat - roiYearsPlat) * 12);
     dispatch({
-      type: "SET_ROI",
-      payload: { years: roiYears, months: roiMonths },
+      type: "SET_ROI_ELEC_PLAT",
+      payload: { years: roiYearsPlat, months: roiMonthsPlat },
+    });
+    // ROI ELEC THERMO
+    const deltaPrixThermo = prixBallonThermo - prixBallonElec;
+    const economieAnnuelleThermo = coutElec - coutThermo;
+    const roiFloatThermo = parseFloat(
+      (deltaPrixThermo / economieAnnuelleThermo).toFixed(2)
+    );
+    const roiYearsThermo = Math.floor(roiFloatThermo);
+    const roiMonthsThermo = Math.round((roiFloatThermo - roiYearsThermo) * 12);
+    dispatch({
+      type: "SET_ROI_ELEC_THERMO",
+      payload: { years: roiYearsThermo, months: roiMonthsThermo },
+    });
+    // ROI PLAT THERMO
+    const deltaPrixPlatThermo = prixBallonThermo - prixBallonPlat;
+    const economieAnnuellePlatThermo = coutPlat - coutThermo;
+    const roiFloatPlatThermo = parseFloat(
+      (deltaPrixPlatThermo / economieAnnuellePlatThermo).toFixed(2)
+    );
+    const roiYearsPlatThermo = Math.floor(roiFloatPlatThermo);
+    const roiMonthsPlatThermo = Math.round(
+      (roiFloatPlatThermo - roiYearsPlatThermo) * 12
+    );
+    dispatch({
+      type: "SET_ROI_PLAT_THERMO",
+      payload: { years: roiYearsPlatThermo, months: roiMonthsPlatThermo },
     });
   };
   return (
-    <main className="flex min-h-screen flex-col w-full md:max-w-md max-w-full p-4 gap-8">
+    <main className="flex min-h-screen flex-col w-full md:max-w-md max-w-full p-4 gap-8 overflow-auto">
       <form className="flex flex-col items-start gap-4" onSubmit={handleSubmit}>
         <Input
           label="Volume ballon (L)"
@@ -167,6 +239,13 @@ const Home = () => {
           }
         />
         <Input
+          label="Consommation ballon plat (kWh/an)"
+          initialValue={state.kwhAnnuelPlat}
+          onChange={(value) =>
+            dispatch({ type: "SET_KWH_ANNUEL_PLAT", payload: value })
+          }
+        />
+        <Input
           label="COP"
           initialValue={state.cop}
           step={0.01}
@@ -177,6 +256,13 @@ const Home = () => {
           initialValue={state.prixBallonElec}
           onChange={(value) =>
             dispatch({ type: "SET_PRIX_BALLON_ELEC", payload: value })
+          }
+        />
+        <Input
+          label="Prix ballon plat (€)"
+          initialValue={state.prixBallonPlat}
+          onChange={(value) =>
+            dispatch({ type: "SET_PRIX_BALLON_PLAT", payload: value })
           }
         />
         <Input
@@ -193,23 +279,52 @@ const Home = () => {
           Calcul
         </button>
       </form>
-      {state.coutElec !== undefined &&
+      {state.kwhElec !== undefined &&
+        state.coutElec !== undefined &&
+        state.coutPlat !== undefined &&
+        state.kwThermo !== undefined &&
         state.coutThermo !== undefined &&
-        state.roi && (
+        state.roiElecPlat !== undefined &&
+        state.roiElecThermo !== undefined &&
+        state.roiPlatThermo !== undefined && (
           <div className="flex flex-col items-start gap-4">
-            <h2>Résultats</h2>
+            <h2 className="font-medium size-6 w-full">Résultats</h2>
+            <h3 className="font-medium">Chauffe eau classique:</h3>
             <div className="flex flex-row justify-between items-center w-full">
-              <span>Chauffe eau classique:</span>
               <span>{state.coutElec}€/an</span>
+              <span>{state.kwhElec}kWh/an</span>
             </div>
+            <h3 className="font-medium">Chauffe eau plat:</h3>
             <div className="flex flex-row justify-between items-center w-full">
-              <span>Chauffe eau thermodynamique:</span>
+              <span>{state.coutPlat}€/an</span>
+              <span>{state.kwhAnnuelPlat}kWh/an</span>
+            </div>
+            <h3 className="font-medium">Chauffe eau thermodynamique:</h3>
+            <div className="flex flex-row justify-between items-center w-full">
               <span>{state.coutThermo}€/an</span>
+              <span>{state.kwThermo}kWh/an</span>
+            </div>
+            <h2 className="font-medium size-6 w-full">
+              Retour sur investissement
+            </h2>
+            <div className="flex flex-row justify-between items-center w-full">
+              <span>plat depuis elec:</span>
+              <span>
+                {state.roiElecPlat.years} ans, {state.roiElecPlat.months} mois
+              </span>
             </div>
             <div className="flex flex-row justify-between items-center w-full">
-              <span>Retour sur investissement:</span>
+              <span>thermo depuis elec:</span>
               <span>
-                {state.roi.years} ans, {state.roi.months} mois
+                {state.roiElecThermo.years} ans, {state.roiElecThermo.months}{" "}
+                mois
+              </span>
+            </div>
+            <div className="flex flex-row justify-between items-center w-full">
+              <span>thermo depuis plat:</span>
+              <span>
+                {state.roiPlatThermo.years} ans, {state.roiPlatThermo.months}{" "}
+                mois
               </span>
             </div>
           </div>
